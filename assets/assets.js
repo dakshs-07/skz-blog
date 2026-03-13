e19e9698b059ff24065585bbcb2e5cf46558cd59
dakshs-07 <dakshpinjore@gmail.com>
2026-03-13 15:05:52 +0530

    main page design and added assets

---
+++ assets/assets.js
@@ -0,0 +1,33 @@
+import logo from "./logo.png";
+import LeeKnow from "./lee-know.jpg";
+import Maniac from "./maniac.jpg";
+import SZK1 from "./skz-2.jpg";
+import SKZ2 from "./skz-3.jpg";
+import DOIT from "./do-it.jpg";
+
+
+export const assets = {
+  logo,
+};
+
+export const blogdata = [
+  {
+    id: 1,
+    title: "Stray Kids Through The Eras",
+    description: "How the chaotic Stray Kids grew through the years.",
+    date: Date.now(),
+    category: "SKZ",
+    image: SZK1,
+  },
+  {
+    id: 2,
+    title: "Lee Know the Cat Butler",
+    description: "How the chaotic Stray Kids grew through the years.",
+    date: Date.now(),
+    category: "Lee Know",
+    image: LeeKnow,
+  },
+  {
+    id
+  }
+];
