0521687c267120efefb2d2e373666c245c724649
dakshs-07 <dakshpinjore@gmail.com>
2026-03-21 11:46:57 +0530

    changed header spacing

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,9 @@ import Subscription from "./Subscription";
 
 function Header() {
   return (
-    <div className="py-5 px-5 md:px-12 lg:px-28">
+    <div className="py-2 px-5 md:px-12 lg:px-28">
       <div className="flex justify-between items-center">
         <a href="/">
-          {" "}
           <Image
             src={assets.logo}
             alt="blog website logo"
