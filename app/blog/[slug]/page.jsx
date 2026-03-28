f7cabe14b889599a2c02b9ce1458958917471a87
dakshs-07 <dakshpinjore@gmail.com>
2026-03-28 12:07:27 +0530

    blogs pulled from db + properly styled

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,3 +1,6 @@
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+
 const { default: Image } = require("next/image");
 
 export default async function Page({params}){
@@ -10,14 +13,23 @@ export default async function Page({params}){
 
   if (!blog) return <div>Blog not found noooo!</div>;
 
-  return(
-    <div className="min-h-screen flex flex-col items-center">
+  return (
+    <div className="w-full flex flex-col px-4 pt-2 pb-20">
+      <div className="flex items-center justify-start gap-1 left-0 my-2 mx-10">
+        <MoveLeft size={20} />
+        <Link href="/" className="underline hover:cursor-pointer hover:opacity-80">Back to blogs</Link>
+      </div>
       <div className="flex flex-col items-center gap-2">
-        <Image src="/assets/do-it.jpg" alt={blog.title} width={400} height={400} />
-        <h1 className="text-5xl">{blog.title}</h1>
-        <p>{blog.description}</p>
-        <p>{blog.content}</p>
+        <Image
+          src="/assets/do-it.jpg"
+          alt={blog.title}
+          width={400}
+          height={400}
+        />
+        <h1 className="text-5xl tracking-wider">{blog.title}</h1>
+        <p className="text-gray-700">{blog.description}</p>
+        <p className="pt-10">{blog.content}</p>
       </div>
     </div>
-  )
+  );
 }
