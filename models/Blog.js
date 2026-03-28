eb1155f4a16a13166d2a80b4ecfb7c5e74dbcdb6
dakshs-07 <dakshpinjore@gmail.com>
2026-03-28 10:54:36 +0530

    added teh schema

--- models/Blog.js
+++ models/Blog.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 const BlogSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true}, 
-    image: {type:String, required: true},
-    category: {type:String, required: true},
-    slug: {type:String, required: true},
-})
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  content: {type: String, required: true},
+  category: { type: String, required: true },
+  slug: { type: String, required: true },
+});
 
-export default mongoose.model.Blog || mongoose.model("Blog", BlogSchema);
+export default mongoose.models.Blog || mongoose.model("Blog", BlogSchema);
