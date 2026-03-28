1926bbe6c4b0a3fef97b5cea0f33103b7fb88fc4
dakshs-07 <dakshpinjore@gmail.com>
2026-03-28 20:21:26 +0530

    dark mode addedddd

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,14 +1,29 @@
+'use client';
+import { Moon } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Topbar() {
-    const LinkStyle = "hover:cursor-pointer hover:underline hover:opacity-80";
+    const LinkStyle = "hover:cursor-pointer hover:underline hover:opacity-80 text-sm";
+    const [dark, setDark] = useState(false);
+    useEffect(()=>{
+        document.documentElement.classList.toggle("dark", dark)
+    }, [dark]);
   return (
-    <div className="py-1 bg-blue-800 text-white">
-      <div className="flex items-center justify-center gap-6">
-        <Link href="/" className={LinkStyle}>All Blogs</Link>
-        <Link href="/add-blog" className={LinkStyle}>Add A blog</Link>
-        <Link href="/" className={LinkStyle}>Subscribe</Link>
+    <div className="py-1 bg-blue-800 text-white dark:bg-gray-100 dark:text-gray-900">
+      <div className="flex items-center">
+        <div className="flex-1 flex justify-center gap-6">
+          <Link href="/" className={LinkStyle}>
+            All Blogs
+          </Link>
+          <Link href="/add-blog" className={LinkStyle}>
+            Add A blog
+          </Link>
+          <Link href="/" className={LinkStyle}>
+            Subscribe
+          </Link>
+        </div>
+        <Moon onClick={() => setDark(!dark)} className="pr-1 cursor-pointer" />
       </div>
     </div>
   );
