a1ffbbf1a8b34715b1e808523a8edd7564114ac6
dakshs-07 <dakshpinjore@gmail.com>
2026-03-29 12:30:42 +0530

    added skzoo sidebarsss so cuteee

--- app/page.js
+++ app/page.js
@@ -1,12 +1,23 @@
 "use client";
 import BlogList from "@/components/BlogList";
+import { LeftSidebar, RightSideBar } from "@/components/Sidebars";
 import Subscription from "@/components/Subscription";
 
 export default function Home() {
   return (
     <>
-      <Subscription />
-      <BlogList />
+      <div className="flex">
+        <div className="w-1/10">
+          <LeftSidebar />
+        </div>
+        <div className="flex-1">
+          <Subscription />
+          <BlogList />
+        </div>
+        <div className="w-1/10">
+          <RightSideBar />
+        </div>
+      </div>
     </>
   );
 }
---
+++ components/Sidebars.jsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import React from 'react'
+
+export function LeftSidebar() {
+  return (
+    <div>
+      <div className='flex flex-col gap-5 py-10 mx-2'>
+        <Image src="/skzoo/dwakei.png" alt="dwaeki" height={200} width={200} className='rotate-30'/>
+        <Image src="/skzoo/foxin.png" alt="dwaeki" height={200} width={200} className='-rotate-20'/>
+        <Image src="/skzoo/Hyunjin.png" alt="dwaeki" height={200} width={200} className='rotate-45' />
+        <Image src="/skzoo/puppym.png" alt="dwaeki" height={200} width={200} className='-rotate-20'/>
+      </div>
+    </div>
+  );
+}
+
+export function RightSideBar(){
+    return (
+      <div>
+        <div className="flex flex-col gap-5 py-10 mx-2">
+          <Image
+            src="/skzoo/leebit.png"
+            alt="dwaeki"
+            height={200}
+            width={200}
+            className="rotate-30"
+          />
+          <Image
+            src="/skzoo/quokka.jpg"
+            alt="dwaeki"
+            height={200}
+            width={200}
+            className="-rotate-20"
+          />
+          <Image
+            src="/skzoo/bbokari.jpg"
+            alt="dwaeki"
+            height={200}
+            width={200}
+            className="rotate-45"
+          />
+          <Image
+            src="/skzoo/wolfchan.jpg"
+            alt="dwaeki"
+            height={80}
+            width={80}
+          />
+        </div>
+      </div>
+    );
+}
