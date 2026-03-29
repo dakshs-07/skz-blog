98647e9f095914f0ad8eaf935e1fd56100b1e9fd
dakshs-07 <dakshpinjore@gmail.com>
2026-03-29 12:37:53 +0530

    changed banner and added links to pages

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,8 +5,8 @@ import { ArrowRight } from "lucide-react";
 
 function Header() {
   return (
-    <div className="relative py-2 px-5 md:px-12 lg:px-28">
-      <Image src="/banner1.jpg" alt="bg" fill className="object-cover -z-10" />
+    <div className="relative py-30 px-5 md:px-12 lg:px-28">
+      <Image src="/banner3.webp" alt="bg" fill className="object-cover -z-10" />
       <div className="flex justify-between items-center">
         <a href="/">
           <Image
@@ -18,13 +18,13 @@ function Header() {
           />
         </a>
         <div className="flex flex-col gap-5">
-          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000] hover:cursor-pointer bg-white">
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#98b5b3] hover:cursor-pointer bg-white">
             Get Started
             <span>
               <ArrowRight />
             </span>
           </button>
-          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000001] hover:cursor-pointer bg-white">
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#98b5b3] hover:cursor-pointer bg-white">
             Add a blog
             <span>
               <ArrowRight />
